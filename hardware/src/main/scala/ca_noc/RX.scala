467230095fa4893579e8479aba1a03993fc89bc2
napoleonwar <60923867+napoleonwar@users.noreply.github.com>
2020-04-30 23:49:20 +0200

    Add files via upload

--- hardware/src/main/scala/ca_noc/RX.scala
+++ hardware/src/main/scala/ca_noc/RX.scala
@@ -1,61 +1,83 @@
 package RX
+import Chisel.Enum
 import NetworkInterface._
+import chisel3._
+import com.sun.org.apache.bcel.internal.generic.NEW
 
-class RX() extends Module {
-    val io = IO(new Bundle{
-      val data_from_router = Input(UInt(105.W))
-      val reqFromRouter = Input(Bool())
-      val ack = Output(Bool())
-      val data_to_OCP = Output(UInt(96.W))
-      val req = Output(Bool())
-      val ackFromOcp = Input(Bool())
-    })
-  val idle :: s1 :: s2 :: s3 :: s4 :: s5 :: Nil = Enum(6)
-  val stateReg = RegInit(idle)
+class WriterIo(size : Int) extends Bundle{
+  val write = Input(Bool())
+  val full = Output(Bool())
+  val din = Input(UInt(size.W))
+}
+class ReadIo(size : Int) extends Bundle{
+  val read = Input(Bool())
+  val empty = Output(Bool())
+  val dout = Output(UInt(size.W))
+}
+/*
+* fifoIN    -> dataReg ->Mux -> fifoOut
+*         width   ^           |
+*                 |___________v
+*
+* */
+class fifoblock(width:Int) extends Module{
+  val io = IO(new Bundle {
+    val fifoIN = new WriterIo(width)
+    val fifoOUT = new ReadIo((width))
+  })
+  val empty :: full :: Nil = Enum(2)
+  val stateReg = RegInit(empty)
+  val dataReg = RegInit(0.U(width.W))
+  val datamux = Mux(io.fifoOUT.read,dataReg,datamux)
+  io.fifoIN.full := ( stateReg === full)
+  io.fifoOUT. empty := ( stateReg === empty)
+  //io.fifoOUT.dout := dataReg
+  io.fifoOUT.dout := datamux
+
+  when( stateReg === empty) {
+    when(io.fifoIN.write) {
+      stateReg := full
+      dataReg := io.fifoIN.din
+    }
+  }. elsewhen ( stateReg === full) {
+    when(io.fifoOUT.read) {
+      stateReg := empty
+      dataReg := 0.U // just to better see empty slots in the waveform
+    }
+  }. otherwise {
+    // There should not be an otherwise state
+  }
+
+}
+class dataprocess(width:Int) extends Module{
+  val io = IO(new Bundle{
+    val data_in = Input(width.W)
+    val data_out = Output((width-3).W)
+  })
+
+}
+/*rx---------------------------------------------------
+* |  Router -> IN -> BLOCK1 -> BLOCK2 -> .... -> BLOCK_N -> OUT -> OCP INTERFACE  |
+* (32+3)*3  abandon the 3 bits phit type store the 32*3 into fifo and ready to transmit into Patmos through OCP IO
+* */
+class RX(/*width: Int,*/depth: Int) extends Module {
+  val width = 35
+  val io = IO(new Bundle{
+      val rxIn = new WriterIo(width)
+      val rxOut = new ReadIo(width-3)
+      })
+
   val dataReg = RegInit(0.U(105.W))
   val countReg = RegInit(0.U(2.W))
 
-  val dataHead = RegInit(0.U(32.W))
-  val dataPayload1 = RegInit(0.U(32.W))
-  val dataPayload2 = RegInit(0.U(32.W))
-  val data  = RegInit(0.U(96.W))
-  data = dataHead + dataPayload1 + dataPayload2
-  dataReg = data_from_router
-
-  val phitType0 = dataReg(32,34)
-  val phitType1 = dataReg(69,67)
-  val phitType2 = dataReg(104,102)
-
-  switch(stateReg){
-    is(idle){
-      when(reqFromRouter){
-        stateReg := s1
-      }
-    }
-    is(s1){
-        if(phitType2 === '101'){
-          if(phitType1 === '100'){
-            if(phitType0 === '110'){
-                dataHead := dataReg(101,70)
-                dataPayload1 := dataReg(66,35)
-                dataPayload2 := dataReg(31,0)
-            stateReg := s2
-            }
-          }
-        }
-      ack = '1'
-    }
-    is(s2){
-      req = '1'
-      ack = '0'
-      data_to_OCP := data
-      stateReg := s3
-    }
-    is(s3){
-      if(ackFromOcp === '1'){
-        req = '0'
-        stateReg := idle
-      }
-    }
+  val buffer = Array.fill(depth){
+    Module(new fifoblock(width-3))
   }
+  for (i <- 0 until depth-1){
+    buffer (i + 1).io.fifoIN.din := buffer (i).io.fifoOUT.dout
+    buffer (i + 1).io.fifoIN.write := buffer (i).io.fifoOUT.empty
+    buffer (i).io.fifoOUT.read := buffer (i + 1).io.fifoIN.full
+  }
+  io.rxIn.din(width-4,0) <> buffer(0).io.fifoIN.din
+  io.rxOut.dout <> buffer(depth-1).io.fifoOUT.dout
 }
