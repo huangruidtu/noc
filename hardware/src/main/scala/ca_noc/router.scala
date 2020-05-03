b6520634a64ae40b1662b893140d7dcf07edd352
Huang Rui <huangruitjut@gmail.com>
2020-05-03 08:57:33 +0200

    upadate router.scala adding Xbar part

--- hardware/src/main/scala/ca_noc/router.scala
+++ hardware/src/main/scala/ca_noc/router.scala
@@ -5,9 +5,27 @@ import chisel3.util.Cat
 
 class router(size:Int) extends Module{
   val io = IO(new Bundle{
+    //--------------E Port----------------
+    val router_in_E = new WriterIo(size)
+    val router_out_E = new ReadIo(size)
+   
+   //--------------S Port----------------
+    val router_in_S = new WriterIo(size)
+    val router_out_S = new ReadIo(size)
+   
+   //--------------W Port----------------
+    val router_in_W = new WriterIo(size)
+    val router_out_W = new ReadIo(size)
+    
+    //--------------N Port----------------
     val router_in_N = new WriterIo(size)
     val router_out_N = new ReadIo(size)
+    
+    //--------------Local Port----------------
+    val router_in_L = new WriterIo(size)
+    val router_out_L = new ReadIo(size)
   })
+  
   //--------------HPU-------------------------
   val data_in = RegInit(0.U(size.W))
   val data_after_mux = RegInit(0.U(size.W))
@@ -23,10 +41,70 @@ class router(size:Int) extends Module{
     data_after_mux := Cat(data_in(size-1,16),routeReg)
   }
   //--------------Cross Bar--------------------
-  when(dest ==== "b00".U){
+  //--------------Output-----------------------
+  when(dest === "b0001".U){ //East Port
+    io.router_out_E.dout := data_after_mux
+  }.elsewhen(dest === "b0010".U){ //South Port
+    io.router_out_S.dout := data_after_mux
+  }.elsewhen(dest === "b0100".U){ // West Port
+    io.router_out_W.dout := data_after_mux
+  }.elsewhen(dest === "b1000".U){ // North Port
+    io.router_out_N.dout := data_after_mux
+  }.otherwise{
+    // do nothing
+  }
+    
+  io.router_in_N.full := true.B
+  
+  //--------------Input-----------------------
+  val idle :: in :: parsing :: out :: Nil = Enum(4)
+  val stateReg = RegInit(idle)
+  val in = Input(Bool())
+  in := io.router_in_E.write | io.router_in_S
+   | io.router_in_W.write | io.router_in_N.write
 
+  val xBar_in = RegInit(0.U(size.W))
+
+  when( stateReg === idle ) {
+      when(io.router_in_E.write) {
+        xBar_in := io.router_in_E.din
+      }.elsewhen(io.router_in_S.write) {
+        xBar_in := io.router_in_S.din
+      }.elsewhen(io.router_in_W.write) {
+        xBar_in := io.router_in_W.din
+      }.elsewhen(io.router_in_N.write) {
+        xBar_in := io.router_in_N.din
+      }
+        stateReg := in
+  }.elsewhen (stateReg === parsing){
+        when(xBar_in(33) === 0.U){
+        data_after_mux := xBar_in
+        }.otherwise{
+            dest := routeReg(3,0) //???
+            routeReg := routeReg>>4.U
+            data_after_mux := Cat(data_in(size-1,16),routeReg)
+        }
+        stateReg := out
+  }.elsewhen (stateReg === out) {
+      when(dest === "b0001".U){ //East Port
+          io.router_out_E.dout := data_after_mux
+      }.elsewhen(dest === "b0010".U){ //South Port
+          io.router_out_S.dout := data_after_mux
+      }.elsewhen(dest === "b0100".U){ // West Port
+          io.router_out_W.dout := data_after_mux
+      }.elsewhen(dest === "b1000".U){ // North Port
+          io.router_out_N.dout := data_after_mux
+      }.elsewhen(dest === "b0000".U){ // Local Port
+          io.router_out_L.dout := data_after_mux
+      }.otherwise{
+          // do nothing
+      }
+      stateReg := idle
   }
 
 
-  io.router_in_N.full := true.B
+
+  
+
+
 }
