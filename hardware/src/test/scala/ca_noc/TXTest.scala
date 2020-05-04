36f7bca7d0da34baa0e1d7489ed04cbc31546ae7
napoleonwar <60923867+napoleonwar@users.noreply.github.com>
2020-05-04 23:09:03 +0200

    TXFIFO works

--- hardware/src/test/scala/ca_noc/TXTest.scala
+++ hardware/src/test/scala/ca_noc/TXTest.scala
@@ -3,60 +3,85 @@ import TX._
 import chisel3._
 import chisel3.iotesters.PeekPokeTester
 import org.scalatest._
-object TXTester {
-  val param = Array("--target-dir", "generated", "--generate-vcd-output", "on")
-}
-class TXTester(dut : TX) extends PeekPokeTester(dut) {
+//object TXTester {
+//  val param = Array("--target-dir", "generated", "--generate-vcd-output", "on")
+//}
+class TXFIFOTester(dut : TX_fifo) extends PeekPokeTester(dut) {
   poke(dut.io.txIn.write, true.B)
-  poke(dut.io.txIn.din, 1)
-  step(1)
+  poke(dut.io.txIn.din, value = 0xabc1)
+  poke(dut.io.txOut.read,true.B)
+  step(3)
+  expect(dut.io.txOut.dout,expected = 0xabc1)
+  expect(dut.io.txOut.empty,true.B)
+  println(peek(dut.io.txOut.dout).toString())
+
+  while(peek(dut.io.txOut.empty) != 1){
+    step(1)
+  }
+
   poke(dut.io.txIn.write, true.B)
-  poke(dut.io.txIn.din, 6)
-  step(1)
-  poke(dut.io.txIn.write, false.B)
-  poke(dut.io.txIn.din, 2)
-  step(1)
-  //println(peek(dut.io.txOut.dout).toString())
+  poke(dut.io.txIn.din, value = 0xbbc2)
+  poke(dut.io.txOut.read,true.B)
+  step(3)
+  expect(dut.io.txOut.dout,expected = 0xbbc2)
+  expect(dut.io.txOut.empty,true.B)
+  println(peek(dut.io.txOut.dout).toString())
+
+  while(peek(dut.io.txOut.empty) != 1){
+    step(1)
+  }
+
+  poke(dut.io.txIn.write, true.B)
+  poke(dut.io.txIn.din, value = 0xfda3)
+  poke(dut.io.txOut.read,true.B)
+  step(3)
+  expect(dut.io.txOut.dout,expected = 0xfda3)
+  expect(dut.io.txOut.empty,true.B)
+  println(peek(dut.io.txOut.dout).toString())
 }
-class TXTest extends FlatSpec with Matchers {
-  "TX" should "pass" in {
-    iotesters.Driver.execute(/*TXTester.param*/Array("--target-dir", "generated", "--generate-vcd-output", "on"),
-      () => new TX(depth = 3)) { c =>
-      new TXTester(c)
-    } should be(true)
+
+object TXFIFOTester extends App {
+  chisel3.iotesters.Driver(() => new TX_fifo(depth = 3)) {
+    m => new TXFIFOTester(m)
   }
 }
 
-//package TXTest
-//import TX._
-//import chisel3._
-//import chisel3.iotesters.{PeekPokeTester, Driver, chiselMainTest}
-//import org.scalatest._
-//
-//class TXTest(dut : TX) extends PeekPokeTester(dut) {
-//  poke(dut.io.txIn.write, true.B)
-//  poke(dut.io.txIn.din, 1)
-//  step(1)
-//  poke(dut.io.txIn.write, true.B)
-//  poke(dut.io.txIn.din, 6)
-//  step(1)
-//  poke(dut.io.txIn.write, false.B)
-//  poke(dut.io.txIn.din, 2)
-//  step(1)
-//
-//  println("the result is"+peek(dut.io.txOut.dout).toString())
-//}
-////class WaveformSpec extends FlatSpec with Matchers{
-////  "Waveform" should "pass" in {
-////    Driver.execute(Array("--target-dir","generated","--generate-vcd-output","on"), ()=>
-////        new TX(depth = 3)) { c=>
-////      new TXTest(c)
-////    }should be(true)
-////  }
-////}
-//
-//object TXTest extends App {
-//  chisel3.iotesters.Driver(() => new TX(depth = 3)) {
-//    m => new TXTest(m)
-//  }
-//}
+class TXPROTester(dut : TXProcess) extends PeekPokeTester(dut) {
+  poke(dut.io.txIn.write, true.B)
+  poke(dut.io.txIn.din, value = 0xabc1)
+  poke(dut.io.txOut.read,true.B)
+  step(3)
+  expect(dut.io.txOut.dout,expected = 0xabc1)
+  expect(dut.io.txOut.empty,true.B)
+  println(peek(dut.io.txOut.dout).toString())
+
+  while(peek(dut.io.txOut.empty) != 1){
+    step(1)
+  }
+
+  poke(dut.io.txIn.write, true.B)
+  poke(dut.io.txIn.din, value = 0xbbc2)
+  poke(dut.io.txOut.read,true.B)
+  step(3)
+  expect(dut.io.txOut.dout,expected = 0xbbc2)
+  expect(dut.io.txOut.empty,true.B)
+  println(peek(dut.io.txOut.dout).toString())
+
+  while(peek(dut.io.txOut.empty) != 1){
+    step(1)
+  }
+
+  poke(dut.io.txIn.write, true.B)
+  poke(dut.io.txIn.din, value = 0xfda3)
+  poke(dut.io.txOut.read,true.B)
+  step(3)
+  expect(dut.io.txOut.dout,expected = 0xfda3)
+  expect(dut.io.txOut.empty,true.B)
+  println(peek(dut.io.txOut.dout).toString())
+}
+
+object TXPROTester extends App {
+  chisel3.iotesters.Driver(() => new TXProcess(depth = 3)) {
+    m => new TXPROTester(m)
+  }
+}
