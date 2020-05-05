5074f19639e72bc6161ab7146b8a706c1f2e6f09
napoleonwar <60923867+napoleonwar@users.noreply.github.com>
2020-05-05 12:16:39 +0200

    hpu and cross bar pass

--- src/test/scala/ca_noc/RouterTest.scala
+++ src/test/scala/ca_noc/RouterTest.scala
@@ -1,14 +1,13 @@
-package RouterTest
+package router
 import chisel3._
 import RX.{ReadIo, WriterIo}
 import chisel3.util.{Cat, Enum, is, switch}
 import chisel3.iotesters.{PeekPokeTester, chiselMainTest}
-import router._
-
 
 class RouterTest(dut : router) extends PeekPokeTester(dut) {
-  poke(dut.io.router_in_L.din, 0x0001)
+  poke(dut.io.router_in_L.din, value = 0x501014482L)
   poke(dut.io.router_in_L.write,true.B)
+
   //poke(dut.io.txIn.din, 1)
   //step(1)
   //println(peek(dut.io.txOut.dout).toString())
@@ -22,44 +21,48 @@ object RouterTest extends App {
 }
 
 
+class HPUTest(dut:HPU) extends PeekPokeTester(dut){
+  poke(dut.io.data_in,value = 0x600101234L)
+  step(1)
+  expect(dut.io.sele,expected = 0x4)
+  expect(dut.io.data_out,expected = 0x600101234L)
+  println(peek(dut.io.data_out).toString())
+  println(peek(dut.io.sele).toString())
+}
+
+object HPUTest extends App {
+  chisel3.iotesters.Driver(() => new HPU(size =35)) {
+    m => new HPUTest(m)
+  }
+}
 
 
+class crossbarTest(dut:XBar) extends PeekPokeTester(dut){
+  poke(dut.io.xbar_sele,value = 4)
+  poke(dut.io.xbar_data_in, value = 0x600101234L)
+  step(1)
+  expect(dut.io.xbar_data_out_W,expected = 0x600101234L)
+
+  poke(dut.io.xbar_sele,value = 1)
+  poke(dut.io.xbar_data_in, value = 0x600101234L)
+  step(1)
+  expect(dut.io.xbar_data_out_N,expected = 0x600101234L)
+
+  poke(dut.io.xbar_sele,value = 2)
+  poke(dut.io.xbar_data_in, value = 0x600101234L)
+  step(1)
+  expect(dut.io.xbar_data_out_S,expected = 0x600101234L)
+
+  poke(dut.io.xbar_sele,value = 8)
+  poke(dut.io.xbar_data_in, value = 0x600101234L)
+  step(1)
+  expect(dut.io.xbar_data_out_E,expected = 0x600101234L)
 
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//package RouterTest
-//import router._
-//import chisel3._
-//import chisel3.iotesters.{PeekPokeTester, chiselMainTest}
-//
-//class RouterTest(dut:router) extends PeekPokeTester (dut){
-//  poke(dut.io.router_in_N.write, true.B)
-//  poke(dut.io.router_in_N.din, 0x000abc1)
-//  step(1)
-//  println(peek(dut.io.router_out_N.dout).toString())
-//}
-//
-//object RouterTest extends App {
-//  chisel3.iotesters.Driver(() => new router(size = 35)) {
-//    m => new RouterTest(m)
-//  }
-//}
+object crossbarTest extends App {
+  chisel3.iotesters.Driver(() => new XBar(size =35)) {
+    m => new crossbarTest(m)
+  }
+}
