7ec9fa2a04063d5dfa0eda1dca5285bc534a1aba
napoleonwar <60923867+napoleonwar@users.noreply.github.com>
2020-05-09 00:04:53 +0200

    FINISH THE ROUTER TEST
    EACH COMPONENT TAKES 2 STEPS TO GET THE RESULT

--- src/test/scala/ca_noc/RouterTest.scala
+++ src/test/scala/ca_noc/RouterTest.scala
@@ -5,12 +5,25 @@ import chisel3.util.{Cat, Enum, is, switch}
 import chisel3.iotesters.{PeekPokeTester, chiselMainTest}
 
 class RouterTest(dut : router) extends PeekPokeTester(dut) {
-  poke(dut.io.router_in_L.din, value = 0x501014482L)
+  /*********           Header        ***********/
+  poke(dut.io.router_in_L.din, value = 0x601014482L)
   poke(dut.io.router_in_L.write,true.B)
+  step(2)
+  poke(dut.io.router_out_S.read,true.B)
+  expect(dut.io.router_out_S.dout,0x601010448L)
 
-  //poke(dut.io.txIn.din, 1)
-  //step(1)
-  //println(peek(dut.io.txOut.dout).toString())
+  /*********           PayLoad1        ***********/
+  poke(dut.io.router_in_L.din, value = 0x40bbcabc1L)
+  poke(dut.io.router_in_L.write,true.B)
+  step(2)
+  poke(dut.io.router_out_S.read,true.B)
+  expect(dut.io.router_out_S.dout,0x40bbcabc1L)
+  /*********           PayLoad2        ***********/
+  poke(dut.io.router_in_L.din, value = 0x50bbcabc2L)
+  poke(dut.io.router_in_L.write,true.B)
+  step(2)
+  poke(dut.io.router_out_S.read,true.B)
+  expect(dut.io.router_out_S.dout,0x50bbcabc2L)
 
 }
 
@@ -25,7 +38,21 @@ class HPUTest(dut:HPU) extends PeekPokeTester(dut){
   poke(dut.io.data_in,value = 0x600101234L)
   step(1)
   expect(dut.io.sele,expected = 0x4)
-  expect(dut.io.data_out,expected = 0x600101234L)
+  expect(dut.io.data_out,expected = 0x600100123L)
+  println(peek(dut.io.data_out).toString())
+  println(peek(dut.io.sele).toString())
+
+  poke(dut.io.data_in,value = 0x40010abcdL)
+  step(1)
+  expect(dut.io.sele,expected = 0x4)
+  expect(dut.io.data_out,expected = 0x40010abcdL)
+  println(peek(dut.io.data_out).toString())
+  println(peek(dut.io.sele).toString())
+
+  poke(dut.io.data_in,value = 0x50010abcdL)
+  step(1)
+  expect(dut.io.sele,expected = 0x4)
+  expect(dut.io.data_out,expected = 0x50010abcdL)
   println(peek(dut.io.data_out).toString())
   println(peek(dut.io.sele).toString())
 }
@@ -64,5 +91,42 @@ class crossbarTest(dut:XBar) extends PeekPokeTester(dut){
 object crossbarTest extends App {
   chisel3.iotesters.Driver(() => new XBar(size =35)) {
     m => new crossbarTest(m)
+  }
+}
+
+class memoryTest(dut : memory_sele) extends PeekPokeTester(dut){
+  // header
+  poke(dut.io.sele,value = 0xa)
+  poke(dut.io.phit_type,value = "b110".U)
+  step(1)
+  expect(dut.io.out_sele,expected = 0xa)
+  // payload1
+  poke(dut.io.phit_type,value = "b100".U)
+  step(1)
+  expect(dut.io.out_sele,expected = 0xa)
+  // payload2
+  poke(dut.io.phit_type,value = "b101".U)
+  step(1)
+  expect(dut.io.out_sele,expected = 0xa)
+  // second package
+  // header
+  poke(dut.io.sele,value = 0xb)
+  poke(dut.io.phit_type,value = "b110".U)
+  step(1)
+  expect(dut.io.out_sele,expected = 0xb)
+  // payload1
+  poke(dut.io.phit_type,value = "b100".U)
+  step(1)
+  expect(dut.io.out_sele,expected = 0xb)
+  // payload2
+  poke(dut.io.phit_type,value = "b101".U)
+  step(1)
+  expect(dut.io.out_sele,expected = 0xb)
+
+}
+
+object memoryTest extends App {
+  chisel3.iotesters.Driver(() => new memory_sele) {
+    m => new memoryTest(m)
   }
 }
