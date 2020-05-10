4d9473fc395c062efb7887735338169d4ae61018
Huang Rui <huangruitjut@gmail.com>
2020-05-10 09:27:57 +0200

    add Ocp Input test

--- src/test/scala/ca_noc/OcpTest.scala
+++ src/test/scala/ca_noc/OcpTest.scala
@@ -7,20 +7,18 @@ import RX.{ReadIo, WriterIo}
 import chisel3.iotesters.{PeekPokeTester, chiselMainTest}
 
 class OcpTest(dut : OcpInterface) extends PeekPokeTester(dut) {
-    //poke(dut.io.txIn.write, true.B)
-    //poke(dut.io.txIn.din, 1)
-    //step(1)
-    //println(peek(dut.io.txOut.dout).toString())
-
     //---------------Patmos->Ocp->OcpOutput--------
-    poke(dut.io.CorePort.M.Cmd, 0x1)
-        poke(dut.io.CorePort.M.Addr, 0x0001)
-        poke(dut.io.CorePort.M.Data, 0x0001)
-        step(3)
-        println(peek(dut.io.addr).toString())
-        println(peek(dut.io.OcpOut.dout).toString())
-
-        //---------------OccpInput->Ocp->Patmos--------
+    //poke(dut.io.CorePort.M.Cmd, 0x1)
+    //poke(dut.io.CorePort.M.Addr, 0x0001)
+    //poke(dut.io.CorePort.M.Data, 0x0001)
+    //step(2)
+    //println(peek(dut.io.addr).toString())
+    //println(peek(dut.io.OcpOut.dout).toString())
+    step(1)
+    //---------------OccpInput->Ocp->Patmos--------
+    poke(dut.io.OcpIn.din, 0x0001)
+    step(2)
+    println(peek(dut.io.CorePort.S.Data).toString())
 
 }
 
